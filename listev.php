96229bf41652948a4a82280b349092af08f96505
Fernando Perry <fernandoperry1234@gmail.com>
2025-01-26 15:56:36 +0700

    fix: now listevent 100% complete (can join)

--- listev.php
+++ listev.php
@@ -14,7 +14,41 @@ include 'koneksi.php';
 $particapated = array();
 $available = array();
 
-$query = "select * from event_participants where user_id = ".$user_id;
+/*$query = "select * from event_participants where user_id = ".$user_id;*/
+$query = "
+SELECT 
+    e.event_id, 
+    e.poster_url, 
+    e.event_name, 
+    e.background_online_url, 
+    e.title, 
+    e.description, 
+    e.date, 
+    e.start_time, 
+    e.end_time, 
+    e.type, 
+    e.link, 
+    e.speaker, 
+    e.published, 
+    e.is_internal, 
+    e.status AS event_status, 
+    e.attendance_type, 
+    e.slug, 
+    e.remark, 
+    ep.event_participant_id, 
+    ep.user_id, 
+    ep.status AS participant_status, 
+    ep.event_role, 
+    ep.certificate_url
+FROM 
+    event_participants ep
+JOIN 
+    events e 
+ON 
+    ep.event_id = e.event_id
+WHERE 
+    ep.user_id =".$user_id;
+
 $result = mysqli_query($koneksi, $query);
 $result_len = mysqli_num_rows($result);
 if ($result_len > 0) {
@@ -49,7 +83,10 @@ if ($result_len > 0) {
                 echo "<tr><td colspan='2' style='text-align: center;'>Tidak ada event yang diikuti.</td></tr>";
             } else {
                 foreach ($particapated as $entry) {
-                    var_dump($entry);
+                    echo "<tr>";
+                    echo "<td>" . htmlspecialchars($entry['title']) . "</td>";
+                    echo "<td>" . htmlspecialchars($entry['speaker']) . "</td>";
+                    echo "</tr>";
                 }
             }
 ?>
@@ -70,7 +107,7 @@ if ($result_len > 0) {
                     echo "<tr>";
                     echo "<td>" . htmlspecialchars($entry['title']) . "</td>";
                     echo "<td>" . htmlspecialchars($entry['speaker']) . "</td>";
-                    echo "<td><button>Daftar</button></td>";
+                    echo "<td><form method='POST' action='masuk-web.php'><input type='text' name='eventid' hidden value='".$entry['event_id']. "'><input type='submit' name='daftar' value='Daftar'></form></td>";
                     echo "</tr>";
                 }
             }
